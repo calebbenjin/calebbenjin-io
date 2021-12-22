4c6741ab9993178024cd38a008485667489612b9
calebbenjin <calebben94@gmail.com>
2021-12-22 10:53:18 +0100

    BlogCard, BlogList, ProjectCard, ProjectList Created

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,6 @@
 import type { NextPage } from 'next'
-import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
-import Stack from '../components/Stack'
 import Footer from '../components/Footer'
-import Projects from '../components/Projects'
-import BlogSection from './BlogSection'
 
 const Layout: NextPage = ({children}) => {
   return (
