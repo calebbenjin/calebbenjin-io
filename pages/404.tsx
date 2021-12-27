56649e9d721dfaee635c6900345a06d7487dd6e3
calebbenjin <calebben94@gmail.com>
2021-12-27 13:34:30 +0100

    Markdown integration

--- pages/404.tsx
+++ pages/404.tsx
@@ -11,8 +11,8 @@ const NotFoundPage = () => {
         <div>
           <Heading>451 Unavailable For Legal Reasons</Heading>
           <p>Why show a generic 404 when I can make it sound mysterious? It seems
-          you've found something that used to exist, or you spelled something
-          wrong. I'm guessing you spelled something wrong. Can you double check
+          you&apos;ve found something that used to exist, or you spelled something
+          wrong. I&apos;m guessing you spelled something wrong. Can you double check
           that URL?</p>
           <div className="link">
             <Link href="/">
